b82dda4537bb3c644ce44dbd00a7aba19bb7d57c
Gabriel Galán Pelayo <gabriel@MacBook-Pro-de-Gabriel.local>
2016-09-19 17:08:17 +0200

    Revert "Stabilize schema.db file"
    This reverts commit 39899d51a56e8b5ce245dcd7f0b17782db417456.

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20160910190345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "unaccent"
   enable_extension "pg_trgm"
 
   create_table "activities", force: :cascade do |t|
