8233f8b5899e37aa79d02074a815555a473a27b9
ggalancs <ggalancs@yahoo.com>
2017-02-01 17:04:44 +0100

    Revert "Add enquiries_count field to tags table"
    This reverts commit 4efa5c959f9dcd26d1f5a1e2ff561502fbd4c7da.

--- db/migrate/20170201145952_add_enquiries_counter_to_tags.rb
+++
@@ -1,6 +0,0 @@
-class AddEnquiriesCounterToTags < ActiveRecord::Migration
-  def change
-    add_column :tags, :enquiries_count, :integer, default: 0
-    add_index :tags, :enquiries_count
-  end
-end
