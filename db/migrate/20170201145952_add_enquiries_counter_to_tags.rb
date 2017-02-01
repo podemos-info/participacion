4efa5c959f9dcd26d1f5a1e2ff561502fbd4c7da
ggalancs <ggalancs@yahoo.com>
2017-02-01 16:55:50 +0100

    Add enquiries_count field to tags table

---
+++ db/migrate/20170201145952_add_enquiries_counter_to_tags.rb
@@ -0,0 +1,6 @@
+class AddEnquiriesCounterToTags < ActiveRecord::Migration
+  def change
+    add_column :tags, :enquiries_count, :integer, default: 0
+    add_index :tags, :enquiries_count
+  end
+end
